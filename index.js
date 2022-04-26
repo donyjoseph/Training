62a61c65aed819176a43fcda91d853d9679c4240
donyjoseph <103090208+donyjoseph@users.noreply.github.com>
2022-04-26 13:36:37 +0530

    Add files via upload
    Add product

--- index.js
+++ index.js
@@ -2,15 +2,13 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 var mysql = require("mysql");
-var con = mysql.createConnection(
-    {
-        host: "localhost",
-        user: "root",
-        password: "password",
-        database: "country"
-    });
-app.post('/updateprofile', function (req, res) {
-
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "country"
+});
+app.post('/addproduct', function (req, res) {
     con.connect(function (err) {
         if (err) {
             console.log(err);
@@ -18,21 +16,20 @@ app.post('/updateprofile', function (req, res) {
         else {
             console.log("Connected");
         }
-          var sql = "update tbluserss set txtFirstName='Biju' where id=1";
+        var sql = "insert into tblproduct (txtProdName,txtProdPrice)values('test',45)";
 
         con.query(sql, function (err, result) {
+
             if (err) {
                 console.log(err);
             }
             else {
-                console.log("edited product!!!!");
+                console.log("Data insert!!!!");
                 res.send(result);
             }
-
         });
     });
-
 })
-app.listen(7000, () => {
+app.listen(5000, () => {
     console.log("Server is connected!!!!");
 })
